31875fd4f75169372b04f3ffda9b5f666bed0daf
LokTruong <truong18@purdue.edu>
2021-10-18 11:50:19 -0400

    implemented emotional experience feedback and added to 'Add Your Story' button.

--- src/script.js
+++ src/script.js
@@ -11,3 +11,19 @@ var msnry = new Masonry( grid, {
 var msnry = new Masonry( '.grid', {
   // options...
 });
+
+// prevent item overlap on unloaded images for Masonry.js
+// init Masonry
+var $grid = $('.grid').masonry({
+  // options...
+});
+// layout Masonry after each image loads
+$grid.imagesLoaded().progress( function() {
+  $grid.masonry('layout');
+});
+
+$('.grid').masonry( {
+  itemSelector: '.grid-item',
+  columnWidth: 160,
+  gutter: 20
+});
